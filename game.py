2882dc119ff9acc56c54cb28ed141465957c5c52
SeanIvers <seanivers55@gmail.com>
2021-12-01 11:57:13 -0500

    Add hit card

--- game.py
+++ game.py
@@ -16,9 +16,40 @@ def play_game():
         print("Your hand is:")
         card_1 = black_jack_deck.pick_random_card()
         card_2 = black_jack_deck.pick_random_card()
-        
+        score = card_1.point_val + card_2.point_val
+        print("Your score is: ", score)
+        print("--------------------------")
+        print("The dealer's hand is:")
+        dealer_card_1 = black_jack_deck.pick_random_card()
+        dealer_card_2 = black_jack_deck.pick_random_card()
+        dealer_score = dealer_card_1.point_val + dealer_card_2.point_val
+        print("The dealer's score is: ", dealer_score)
 
-        choice = input("Would you like to play a hand of Black Jack? y or n ")
+        if dealer_score == 21:
+            print("Sorry, you lose.")
+            break
+
+        elif score == 21:
+            print("You win!")
+            break
+        
+        hit = input("Would you like to hit? y or n ")
+        while hit == "y":
+            card = black_jack_deck.pick_random_card()
+            score += card.point_val
+            print(card.point_val)
+            print(score)
+
+            if score == 21:
+                print("You win!")
+
+            elif score > 21:
+                print("Sorry, you lose.")
+                break
+
+            hit = input("Would you like to hit? y or n ")
+
+        choice = input("Would you like to play another hand of Black Jack? y or n ")
     
     if choice == "n":
         print("Thanks for playing!")
